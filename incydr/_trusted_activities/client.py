74359b5d9691a6f209e3a569ad255be5b289e2a4
tim.abramson <tim.abramson@code42.com>
2022-07-22 10:47:30 -0500

    reset to empty project so we get a clean POC PR

--- incydr/_trusted_activities/client.py
+++
@@ -1,8 +0,0 @@
-from requests import Session
-
-
-class TrustedActivitiesClient:
-    default_page_size = 100
-
-    def __init__(self, session: Session):
-        self._session = session
--- tests/__init__.py
+++
@@ -1,3 +0,0 @@
-# SPDX-FileCopyrightText: 2022-present Code42 Software <integrations@code42.com>
-#
-# SPDX-License-Identifier: MIT
