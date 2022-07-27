fc98f0cd6e2d2cc3bc35c7900089c9dc779b4c1b
Tora Kozic <tora.kozic@code42.com>
2022-07-27 15:01:49 -0500

    customer client

---
+++ incydr/_customer/models.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+from enum import Enum
+from typing import Optional, List, Tuple
+
+from pydantic import BaseModel, Field
+
+from incydr._core.models import ResponseModel
