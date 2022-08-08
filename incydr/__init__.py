634984709684140ccf7c3acee0b871f7a171fdd1
tim.abramson <tim.abramson@code42.com>
2022-08-08 15:53:51 -0500

    uncomment public API module rename

--- incydr/__init__.py
+++ incydr/__init__.py
@@ -3,9 +3,9 @@
 # SPDX-License-Identifier: MIT
 from incydr._core.client import Client
 
-# __all__ = ["Client"]
-#
-# __locals = locals()
-# for __name in __all__:
-#     if not __name.startswith("__"):
-#         setattr(__locals[__name], "__module__", "incydr")  # noqa
+__all__ = ["Client"]
+
+__locals = locals()
+for __name in __all__:
+    if not __name.startswith("__"):
+        setattr(__locals[__name], "__module__", "incydr")  # noqa
