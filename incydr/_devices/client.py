f47cc5846ea831335d116b787d1e06c32ca443b4
Tora Kozic <tora.kozic@code42.com>
2022-08-10 14:00:17 -0500

    add doc strings for device functions and models

--- incydr/_devices/client.py
+++ incydr/_devices/client.py
@@ -21,7 +21,14 @@ class DevicesClient:
 
 
 class DevicesV1:
-    """Devices V1 Client"""
+    """Client for `/v1/devices` endpoints.
+
+    Usage example:
+
+        >>> import incydr
+        >>> client = incydr.Client(**kwargs)
+        >>> client.devices.v1.get_page()
+    """
 
     default_page_size = 100
 
@@ -29,7 +36,15 @@ class DevicesV1:
         self._session = session
 
     def get_device(self, device_id: str) -> Device:
-        """Get a single device."""
+        """Get a single device.
+
+        **Parameters:**
+
+        * **device_id**: `str` (required) - The unique ID for the device.
+
+        **Returns**: A [`Device`][device-model] object representing the device.
+
+        """
         response = self._session.get(f"/v1/devices/{device_id}")
         return Device.parse_response(response)
 
@@ -42,7 +57,21 @@ class DevicesV1:
         sort_dir: SortDirection = SortDirection.ASC,
         sort_key: SortKeys = SortKeys.NAME,
     ) -> DevicesPage:
-        """Get a page of devices."""
+        """
+        Get a page of devices.
+
+        Filter results by passing the appropriate parameters:
+
+        **Parameters**:
+        * **active**: `bool` -
+        * **blocked**: `bool` -
+        * **page_num**: `int` -
+        * **page_size**: `int` -
+        * **sort_dir**: `SortDirection` - 'asc' or 'desc'. The direction in which to sort the response based on the corresponding key. Defaults to 'asc'.
+        * **sort_key**: `SortKeys` - One or more values on which the response will be sorted. Defaults to device name.
+
+        **Returns**: A ['DevicesPage'][devicespage-model] object.
+        """
 
         data = QueryDevicesRequest(
             page=page_num,
@@ -63,7 +92,13 @@ class DevicesV1:
         sort_dir: SortDirection = SortDirection.ASC,
         sort_key: SortKeys = SortKeys.NAME,
     ) -> Iterator[Device]:
-        """Iterate over all devices"""
+        """
+        Iterate over all devices.
+
+        Accepts the same parameters as `.get_page()` excepting `page_num`.
+
+        **Returns**: A generator yielding individual [`Device`][device-model] objects.
+        """
         for page_num in count(1):
             page = self.get_page(
                 active=active,
