a7444b992dfa0c2caa419387145253fae28ca54e
Tora Kozic <tora.kozic@code42.com>
2022-10-20 10:38:01 -0500

    add departments commands

--- incydr/cli/main.py
+++ incydr/cli/main.py
@@ -4,6 +4,7 @@ from incydr.cli import init_client
 from incydr.cli import log_file_option
 from incydr.cli import log_level_option
 from incydr.cli.cmds.cases import cases
+from incydr.cli.cmds.departments import departments
 from incydr.cli.cmds.file_events import file_events
 from incydr.cli.core import IncydrGroup
 
@@ -17,6 +18,7 @@ def incydr(ctx, log_level, log_file):
     pass
 
 
+incydr.add_command(departments)
 incydr.add_command(file_events)
 incydr.add_command(cases, name="cases")
 
