da75913e881d0c0b36c0eb1ebddd387841ea2fc8
tim.abramson <tim.abramson@code42.com>
2022-11-08 08:57:31 -0600

    update table/csv renderers

--- incydr/cli/render.py
+++ incydr/cli/render.py
@@ -1,12 +1,28 @@
+import os
+import sys
+import platform
+from csv import DictWriter
 from datetime import datetime
 from itertools import chain
 from typing import Iterable
 from typing import Set
+from typing import Type
 
 from pydantic import BaseModel
 from rich.table import Table
+from rich.console import RenderableType
 
+from incydr.utils import get_fields
 from incydr.cli import console
+from incydr.utils import iter_model_formatted
+from incydr.utils import model_as_card
+from incydr._core.models import Model
+
+if platform.system() in ("Darwin", "Linux"):
+    os.environ["MANPAGER"] = "less -S"
+else:
+    ...
+    # TODO: figure out Windows pager to use
 
 
 def date(dt: datetime):
@@ -19,24 +35,38 @@ def date_time(dt: datetime):
     return dt.isoformat(sep=" ")[:16] + " UTC"
 
 
-def model_as_row(model: BaseModel, columns: Set[str] = None):
-    for name, value in model:
-        if columns and name not in columns:
-            continue
-        if isinstance(value, datetime):
-            yield date_time(value)
-        else:
-            yield str(value)
+def table(
+    model: Type[Model],
+    models: Iterable[Model],
+    columns: list[str] = None,
+    title=None,
+    flat=False,
+):
+    headers = list(get_fields(model, include=columns, flat=flat))
+    header_padding = len(headers) * 3
+    max_width = 0
+    tbl = Table(*headers, title=title, show_lines=True)
+    for m in models:
+        values = []
+        row_width = 0
+        for name, value in iter_model_formatted(
+            m, include=headers, flat=flat, render="table"
+        ):
+            if isinstance(value, BaseModel):
+                value = model_as_card(value)
+            elif not isinstance(value, RenderableType):
+                value = str(value)
+            value_size = console.measure(value).maximum
+            row_width += value_size
+            values.append(value)
+        if row_width + header_padding > max_width:
+            max_width = row_width + header_padding
+        tbl.add_row(*values)
 
-
-def table(models: Iterable[BaseModel], columns: Set[str] = None, title=None):
-    models = iter(models)
-    first = next(models)
-    header = first.dict(by_alias=False, include=columns).keys()
-    tbl = Table(*header, title=title)
-    for model in chain([first], models):
-        tbl.add_row(*model_as_row(model, columns=columns))
-    console.print(tbl)
+    console.width = max_width
+    tbl.width = max_width
+    with console.pager():
+        console.print(tbl, soft_wrap=False)
 
 
 def table_json(results: Iterable, columns: Set[str] = None, title=None):
@@ -55,3 +85,20 @@ def table_json(results: Iterable, columns: Set[str] = None, title=None):
                 row.append(str(v))
         tbl.add_row(*row)
     console.print(tbl)
+
+
+def csv(
+    model: Type[Model],
+    models: Iterable[Model],
+    columns: list[str] = None,
+    flat: bool = False,
+):
+    headers = list(get_fields(model, columns, flat=flat))
+    writer = DictWriter(sys.stdout, fieldnames=headers, extrasaction="ignore")
+
+    writer.writeheader()
+    for m in models:
+        flat_event = dict(
+            iter_model_formatted(m, flat=flat, render="csv", include=headers)
+        )
+        writer.writerow(flat_event)
