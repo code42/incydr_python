37ee43396fe927dec5821962b84ef5bc811062b9
tim.abramson <tim.abramson@code42.com>
2023-04-21 08:44:23 -0500

    some docs cleanup

--- src/_incydr_sdk/enums/cases.py
+++ src/_incydr_sdk/enums/cases.py
@@ -2,6 +2,7 @@ from _incydr_sdk.enums import _Enum
 
 
 class SortKeys(_Enum):
+    """Possible keys to sort cases list results by."""
     NAME = "name"
     NUMBER = "number"
     CREATED_AT = "createdAt"
@@ -12,12 +13,14 @@ class SortKeys(_Enum):
 
 
 class CaseStatus(_Enum):
+    """Possible statuses for a case."""
     CLOSED = "CLOSED"
     OPEN = "OPEN"
     ARCHIVED = "ARCHIVED"
 
 
 class FileAvailability(_Enum):
+    """Possible states of file-event source file availability."""
     error = "ERROR"
     exact_file_available = "EXACT_FILE_AVAILABLE"
     no_file_available = "NO_FILE_AVAILABLE"
